edd83b4ae0e9541b7e9fbdb8f33adf608f9f02b6
kwea123 <kwea123@gmail.com>
2022-07-03 21:57:01 +0900

    upd

--- benchmarking/benchmark_synthetic_nsvf.sh
+++ benchmarking/benchmark_synthetic_nsvf.sh
@@ -4,21 +4,21 @@ python train.py \
     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Wineholder \
     --exp_name Wineholder
 
-# python train.py \
-#     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Steamtrain \
-#     --exp_name Steamtrain
+python train.py \
+    --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Steamtrain \
+    --exp_name Steamtrain
 
-# python train.py \
-#     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Toad \
-#     --exp_name Toad
+python train.py \
+    --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Toad \
+    --exp_name Toad
 
 python train.py \
     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Robot \
     --exp_name Robot
 
-# python train.py \
-#     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Bike \
-#     --exp_name Bike
+python train.py \
+    --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Bike \
+    --exp_name Bike
 
 python train.py \
     --root_dir /home/ubuntu/data/nerf_data/Synthetic_NSVF/Palace \
