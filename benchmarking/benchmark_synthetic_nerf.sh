376d758a7d5fb41521dfa8228be8cb2504007fe9
kwea123 <kwea123@gmail.com>
2022-07-06 20:20:20 +0900

    upd

--- benchmarking/benchmark_synthetic_nerf.sh
+++ benchmarking/benchmark_synthetic_nerf.sh
@@ -2,35 +2,35 @@
 
 export ROOT_DIR=/home/ubuntu/data/nerf_data/Synthetic_NeRF
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Chair \
-#     --exp_name Chair --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Chair \
+    --exp_name Chair --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Drums \
-#     --exp_name Drums --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Drums \
+    --exp_name Drums --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Ficus \
-#     --exp_name Ficus --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Ficus \
+    --exp_name Ficus --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Hotdog \
-#     --exp_name Hotdog --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Hotdog \
+    --exp_name Hotdog --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Lego \
-#     --exp_name Lego --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Lego \
+    --exp_name Lego --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
-# python train.py \
-#     --root_dir $ROOT_DIR/Materials \
-#     --exp_name Materials --no_save_test \
-#     --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
+python train.py \
+    --root_dir $ROOT_DIR/Materials \
+    --exp_name Materials --no_save_test \
+    --num_epochs 20 --batch_size 16384 --lr 2e-2 --scale 0.5
 
 python train.py \
     --root_dir $ROOT_DIR/Mic \
