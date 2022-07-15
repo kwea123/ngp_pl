47be25d411e2f2b4bdf1e3032a56715ad6a207e6
kwea123 <kwea123@gmail.com>
2022-07-15 22:14:00 +0900

    add back metrics for evaluation in ipynb

---
+++ metrics.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def mse(image_pred, image_gt, valid_mask=None, reduction='mean'):
+    value = (image_pred-image_gt)**2
+    if valid_mask is not None:
+        value = value[valid_mask]
+    if reduction == 'mean':
+        return torch.mean(value)
+    return value
+
+
+@torch.no_grad()
+def psnr(image_pred, image_gt, valid_mask=None, reduction='mean'):
+    return -10*torch.log10(mse(image_pred, image_gt, valid_mask, reduction))
