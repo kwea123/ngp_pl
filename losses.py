b9047360be1f855c02492ffd507c251b37894f2a
kwea123 <kwea123@gmail.com>
2022-07-22 20:07:52 +0900

    add depth loss if disp is provided

--- losses.py
+++ losses.py
@@ -2,18 +2,44 @@ import torch
 from torch import nn
 
 
+def shiftscale_inv_depthloss(disp_pred, disp_gt):
+    """
+    Computes the shift- scale- invariant depth loss as proposed in
+    https://arxiv.org/pdf/1907.01341.pdf.
+    Inputs:
+        disp_pred: (N) disp predicted by the network
+        disp_gt: (N) disparity produced by image-based method.
+    Outputs:
+        loss: (N)
+    """
+    t_pred = torch.median(disp_pred)
+    s_pred = torch.mean(torch.abs(disp_pred-t_pred))
+    t_gt = torch.median(disp_gt)
+    s_gt = torch.mean(torch.abs(disp_gt-t_gt))
+
+    disp_pred_n = (disp_pred-t_pred)/s_pred
+    disp_gt_n = (disp_gt-t_gt)/s_gt
+    loss = (disp_pred_n-disp_gt_n)**2
+    return loss
+
+
 class NeRFLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.lambda_geo = 1e-4
         self.lambda_opa = 1e-3
 
-    def forward(self, results, rgbs, **kwargs):
+    def forward(self, results, target, **kwargs):
         d = {}
-        d['rgb'] = (results['rgb']-rgbs)**2
+        d['rgb'] = (results['rgb']-target['rgb'])**2
 
-        o = results['opacity']+1e-8
+        o = results['opacity']+1e-10
         # encourage opacity to be either 0 or 1 to avoid floater
         d['opacity'] = self.lambda_opa*(-o*torch.log(o))
+
+        if 'disp' in target:
+            d['depth'] = self.lambda_geo * \
+                shiftscale_inv_depthloss(1/(results['depth']+1e-10), target['disp'])
 
         return d
