e483b450996a84ffd50249b867f44eaee017faf8
kwea123 <kwea123@gmail.com>
2022-07-29 09:51:15 +0900

    upd

--- datasets/ray_utils.py
+++ datasets/ray_utils.py
@@ -39,7 +39,7 @@ def get_ray_directions(H, W, K, random=False, return_uv=False, flatten=True):
 
     if return_uv:
         return directions, grid
-    return directions.cuda()
+    return directions
 
 
 @torch.cuda.amp.autocast(dtype=torch.float32)
