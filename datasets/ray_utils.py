fae338aa70fd6682b4b5d029e14f19faf37338ed
kwea123 <kwea123@gmail.com>
2022-08-26 11:29:21 +0900

    fix axisangle

--- datasets/ray_utils.py
+++ datasets/ray_utils.py
@@ -77,11 +77,14 @@ def axisangle_to_R(v):
     from https://github.com/ActiveVisionLab/nerfmm/blob/main/utils/lie_group_helper.py#L47
 
     Inputs:
-        v: (B, 3)
+        v: (3) or (B, 3)
     
     Outputs:
-        R: (B, 3, 3)
+        R: (3, 3) or (B, 3, 3)
     """
+    v_ndim = v.ndim
+    if v_ndim==1:
+        v = rearrange(v, 'c -> 1 c')
     zero = torch.zeros_like(v[:, :1]) # (B, 1)
     skew_v0 = torch.cat([zero, -v[:, 2:3], v[:, 1:2]], 1) # (B, 3)
     skew_v1 = torch.cat([v[:, 2:3], zero, -v[:, 0:1]], 1)
@@ -92,6 +95,8 @@ def axisangle_to_R(v):
     eye = torch.eye(3, device=v.device)
     R = eye + (torch.sin(norm_v)/norm_v)*skew_v + \
         ((1-torch.cos(norm_v))/norm_v**2)*(skew_v@skew_v)
+    if v_ndim==1:
+        R = rearrange(R, '1 c d -> c d')
     return R
 
 
